5e943e3762801cbf63ca3152df4842f677161b4d
Shubham Singh <shubhambaba143143@gmail.com>
2023-04-07 03:56:02 +0530

    ant pro table

---
+++ src/Components/header/Header.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './Header.css';
+import AddTask from '../addTask/AddTask';
+import { IoIosAdd } from "react-icons/io";
+import AntTable from '../../ant/AntTable';
+
+const Header = () => {
+    const [task, setTask] = useState([
+        {
+            startDate: new Date().toLocaleDateString(),
+            dueDate: "2023-04-10 ",
+            tag: "Home",
+            title: "Vegetable",
+            description: " cmweinciweklcw",
+            status: "Open",
+            Key: 0
+        }
+    ]);
+    const [formOpen, setFormOpen] = useState(false);
+
+
+    return <div className='main-head_header'>
+        <div className='starting-state'>
+            <div>
+                <p className='emoji-part'>😎  <span className='set-task-mot'>Let's set a new goal</span></p>
+                <button className='new-task-button' onClick={() => setFormOpen(!formOpen)}><IoIosAdd /> New task</button>
+            </div>
+            <AntTable data={task} setData={setTask} />
+        </div>
+        {formOpen ? <AddTask setTask={setTask} setFormOpen={setFormOpen} /> : ""}
+    </div>
+
+};
+
+export default Header;

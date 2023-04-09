ee2ff3e8c0ccbd66d235007d0300d860debd8c7b
Shubham Singh <shubhambaba143143@gmail.com>
2023-04-10 01:19:19 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-
 import Navbar from './Components/navbar/Navbar';
-// import Home from './Components/Home/Home';
-// import AntTable from "./ant/AntTable";
 import Header from './Components/header/Header';
-// import AddTask from './Components/addTask/AddTask';
 import "./App.css";
 
 
@@ -12,7 +8,6 @@ function App() {
     <div className="App home-main_head">
       <Navbar />
       <Header />
-      {/* <AntTable /> */}
     </div>
   );
 }
